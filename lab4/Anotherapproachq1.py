bb4c383b9571cf1073b9593add778b2448d1d4ea
souravdungbung <lesouravdungbungchamlingrai@gmail.com>
2022-09-01 10:11:29 +0600

    new update

---
+++ lab4/Anotherapproachq1.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+# sigmoid activation 
+def activationFunction(model, type ="sigmoid"):
+    return {
+	"sigmoid": 1 / (1 + np.exp(-model))
+}[type]
+
+# designing perceptron model
+def perceptronModel(weights, inputs, bias):
+    model = np.add(np.dot(inputs, weights), bias)
+    logic = activationFunction(model, type ="sigmoid")
+    return np.round(logic)
+
+# computation model
+def compute(data, logicGate, weights, bias):
+    weights = np.array(weights)
+    output = np.array([ perceptronModel(weights,
+			datum, bias) for datum in data ])
+    return output
+
+# Print Output
+def printOutput(dataset, name, data):
+    print("Logic Function: {}".format(name.upper()))
+    print("X1\tX2\tX3\tY")
+    toPrint = ["{1}\t{2}\t{3}\t{0}".format(output, *datas)
+			for datas, output in zip(dataset, data)]
+    for i in toPrint:
+	    print(i)
+
+# main function
+def main():
+
+    dataset = np.array([
+	    [0, 0, 0],
+	    [0, 0, 1],
+	    [0, 1, 0],
+	    [0, 1, 1],
+	    [1, 0, 0],
+	    [1, 0, 1],
+	    [1, 1, 0],
+	    [1, 1, 1]
+])
+
+
+    logicGate = {
+	"and": compute(dataset, "and", [1, 1, 1], -2),
+	"or": compute(dataset, "or", [1, 1, 1], -0.9),
+	
+}
+    for gate in logicGate:
+	    printOutput(dataset, gate, logicGate[gate])
+
+if __name__ == '__main__':
+    main()
--- lab4/Question1.py
+++ lab4/Question1.py
@@ -1,57 +1,34 @@
-
 import numpy as np
-
-# sigmoid activation 
-def activationFunction(model, type ="sigmoid"):
-    return {
-	"sigmoid": 1 / (1 + np.exp(-model))
-}[type]
-
-# designing perceptron model
-def perceptronModel(weights, inputs, bias):
-    model = np.add(np.dot(inputs, weights), bias)
-    logic = activationFunction(model, type ="sigmoid")
-    return np.round(logic)
-
-# computation model
-def compute(data, logicGate, weights, bias):
-    weights = np.array(weights)
-    output = np.array([ perceptronModel(weights,
-			datum, bias) for datum in data ])
-    return output
-
-# Print Output
-def printOutput(dataset, name, data):
-    print("Logic Function: {}".format(name.upper()))
-    print("X1\tX2\tX3\tY")
-    toPrint = ["{1}\t{2}\t{3}\t{0}".format(output, *datas)
-			for datas, output in zip(dataset, data)]
-    for i in toPrint:
-	    print(i)
-
-# main function
-def main():
-
-    dataset = np.array([
-	    [0, 0, 0],
-	    [0, 0, 1],
-	    [0, 1, 0],
-	    [0, 1, 1],
-	    [1, 0, 0],
-	    [1, 0, 1],
-	    [1, 1, 0],
-	    [1, 1, 1]
-])
+def activation(y): 
+    if y <= 0:
+        return 0 
+    else:
+        return 1 #AND Gate
+def and_gate(x1,x2,x3): 
+    x=np.array([x1,x2,x3])
+    w=np.array([0.5,0.5,0.5]) 
+    b=-1
+    
+    y=np.sum(w*x)+ b 
+    return activation(y)
+def or_gate(x1,x2,x3): 
+    x=np.array([x1,x2,x3]) 
+    w=np.array([0.5,0.5,0.5]) 
+    b=-0.2
+    y=np.sum(w*x)+ b 
+    return activation(y)
 
 
-    logicGate = {
-	"and": compute(dataset, "and", [1, 1, 1], -2),
-	"or": compute(dataset, "or", [1, 1, 1], -0.9),
-	
-}
-    for gate in logicGate:
-	    printOutput(dataset, gate, logicGate[gate])
+arrays=[ [0,0,0],[0,0,1],[0,1,1],[1,1,1],[1,0,0],[1,1,0],[0,1,0],[1,0,1]
+]
 
-if __name__ == '__main__':
-    main()
-
+print("AND GATE") 
+for x1,x2,x3 in arrays:
+    print('input: ',x1,x2,x3,'output' , and_gate(x1,x2,x3))
+print("") 
+print("") 
+print("")
+print("")
+print("OR GATE")
+for x1,x2,x3 in arrays:
+    print('input: ',x1,x2,x3,'output' , or_gate(x1,x2,x3))
